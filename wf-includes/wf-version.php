510a48066ac9f7519044134b20f8fd9f47509133
jonnyauk <jonny@wonderflux.com>
2011-01-16 00:40:58 +0000

    Update trunk versioning to reflect development status.

--- wf-includes/wf-version.php
+++ wf-includes/wf-version.php
@@ -24,6 +24,6 @@ define( 'WF_WORDPRESS_OPTI', '3.0.4' );
  *
  * @global string $wp_version
  */
-define( 'WF_VERSION', '0.902 PRIVATE DEVELOPMENT NIGHTLY' );
+define( 'WF_VERSION', '0.911 SVN TRUNK DEVELOPMENT BETA' );
 
 ?>

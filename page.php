cf24269a28eabe9e5618f8fcb7b71e1ce2144580
jonnyauk <jonny@wonderflux.com>
2011-07-13 18:05:02 +0000

    - Need conditional statements to setup hooks in page.php - this template file is used when a static page is set to homepage (not home.php as you might expect).

--- page.php
+++ page.php
@@ -12,14 +12,19 @@ get_header();
 wfmain_before_wrapper(); //WF display hook
 
 wfmain_before_all_container(); //WF display hook
-wfmain_before_page_container(); //WF display hook
+// This template gets used when a static page is set to home
+if (is_home() || is_front_page()) : wfmain_before_home_container(); //WF display hook
+	else: wfmain_before_page_container(); //WF display hook
+endif;
 ?>
 
 <div class="container" id="main-content">
 
 	<?php // Main content
 	wfmain_before_all_content(); //WF display hook
-	wfmain_before_page_content();
+	if (is_home() || is_front_page()) : wfmain_before_home_content(); //WF display hook
+		else: wfmain_before_page_content(); //WF display hook
+	endif;
 
 	get_template_part( 'loop', 'page' );
 
@@ -29,7 +34,9 @@ wfmain_before_page_container(); //WF display hook
 	wfx_get_sidebar(''); //WF WordPress get_sidebar function replacement
 
 	// Display hooks for after main content and sidebar
-	wfmain_after_page_main_content(); //WF display hook
+	if (is_home() || is_front_page()) : wfmain_after_home_main_content(); //WF display hook
+		else: wfmain_after_page_main_content(); //WF display hook
+	endif;
 	wfmain_after_all_main_content(); //WF display hook
 	?>
 

37bffe01ef94262c74606682884b5a620a8f6402
jonnyauk <jonny@wonderflux.com>
2011-08-02 21:28:03 +0000

    - Update legacy code and improve readme.

--- footer-content.php
+++ footer-content.php
@@ -9,21 +9,12 @@
  *
  * @package Wonderflux
  */
-
-
-/**
- * The core get_template_part footer content
- * This will be over-ridden if you create a file of the same name in your child theme
- *
- * @package Wonderflux
- * @since Wonderflux 0.6
- */
 ?>
 <div <?php wfx_css('size=full&id=footer-content'); ?>>
 
 	<div class="box-footer">
-		<h3><?php echo esc_attr( get_bloginfo( 'description', 'display' ) ); ?></h3>
-		<p><?php /*$wfx-> wf_loginlogout('');*/ ?></p>
+		<h4><?php echo esc_attr( get_bloginfo( 'description', 'display' ) ); ?></h4>
+		<p><?php wfx_login_logout(''); ?></p>
 	</div>
 
 </div>

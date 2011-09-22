a84e7fef2e6bfde230f59ffaca63fed44998be89
jonnyauk <jonny@wonderflux.com>
2011-09-22 13:42:06 +0000

    - Setup the 'system' admin page for translation.

--- wf-includes/admin-pages/wf-page-system.php
+++ wf-includes/admin-pages/wf-page-system.php
@@ -1,21 +1,13 @@
 <?php
-
 $this->wf_latest_version_notice();
-
 global $wpdb;
-$php_version_check = phpversion();
-$mysql_version = $wpdb->db_version();
 
-$output_server = '<h3>Server technical information</h3>';
-$output_server .= '<p>PHP version installed: ' . phpversion() . '<p>';
-$output_server .= '<p>MySQL version installed: ' . $mysql_version . '<p>';
+echo '<h3>'. esc_attr__('Server technical information', 'wonderflux') . '</h3>';
+echo '<p>' . sprintf( __( 'PHP version installed: %s', 'wonderflux' ), phpversion() ) . '</p>';
+echo '<p>' . sprintf( __( 'MySQL version installed: %s', 'wonderflux' ), $wpdb->db_version() ) . '</p>';
 
-$output_tech = '<h3>Wonderflux technical information</h3>';
-$output_tech .= '<p>Wonderflux version: ' . WF_VERSION . ', requires at-least WordPress v' . WF_WORDPRESS_MIN . ', optimised for WordPress v' . WF_WORDPRESS_OPTI . '</p>';
-$output_tech .= '<p>Wonderflux server path: ' . WF_MAIN_DIR . '</p>';
-$output_tech .= '<p>Wonderflux URL path: ' . WF_MAIN_URL . '</p>';
-
-echo $output_server;
-echo $output_tech;
-
+echo '<h3>' . esc_attr__('Wonderflux technical information', 'wonderflux') . '</h3>';
+echo '<p>' . sprintf( __( 'Wonderflux version: %1$s, requires at-least WordPress v%2$s, optimised for WordPress v%3$s', 'wonderflux' ), WF_VERSION, WF_WORDPRESS_MIN, WF_WORDPRESS_OPTI ) . '</p>';
+echo '<p>' . sprintf( __( 'Wonderflux server path: %s', 'wonderflux' ), WF_MAIN_DIR ) . '</p>';
+echo '<p>' . sprintf( __( 'Wonderflux URL path: %s', 'wonderflux' ), WF_MAIN_URL ) . '</p>';
 ?>

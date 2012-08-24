6c2a9c2b6d145b071491a38d18ecf54045081246
jonnyauk <jonny@wonderflux.com>
2012-08-24 09:31:21 +0000

    - Use get_stylesheet_directory_uri() rather than get_bloginfo('stylesheet_directory'). - Required for WordPress theme repo compliance.

--- wf-config.php
+++ wf-config.php
@@ -17,8 +17,8 @@ if (!defined('WF_INCLUDES_URL')) { define( 'WF_INCLUDES_URL', WF_MAIN_URL . '/wf
 
 // Backpat - NOTICE -> Will be removed in Wonderflux v1.1
 // STOP USING WF_THEME - USE WF_THEME_URL INTEAD!
-if (!defined('WF_THEME')) { define( 'WF_THEME', get_bloginfo('stylesheet_directory') ); }
-if (!defined('WF_THEME_URL')) { define( 'WF_THEME_URL', get_bloginfo('stylesheet_directory') ); }
+if (!defined('WF_THEME')) { define( 'WF_THEME', get_stylesheet_directory_uri() ); }
+if (!defined('WF_THEME_URL')) { define( 'WF_THEME_URL', get_stylesheet_directory_uri() ); }
 if (!defined('WF_THEME_DIR')) { define( 'WF_THEME_DIR', get_stylesheet_directory() ); }
 
 if (!defined('WF_ADMIN_ACCESS')) { define( 'WF_ADMIN_ACCESS', 'administrator' ); }

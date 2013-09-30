1c3d40ce8141d3400a50b5e33c751b939153dc98
Jonnyauk <jonny@wonderflux.com>
2013-09-30 08:50:48 +0100

    New BuddyPress v1.8 template files
    All new BuddyPress template support - tested upto v1.9 (current development trunk) and fully compatible with v1.8 (current released version - minimum required).
    
    IMPORTANT - If you wish to override any of these files in your child theme, please duplicate the files and directory structure into your child theme!

---
+++ buddypress/buddypress.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * Core Wonderflux buddypress template
+ * Customise this in your child theme by:
+ * - Using hooks and your own functions
+ * - Copying this file to your child theme - it will over-ride this file
+ * - IMPORTANT: DUPLICATE DIRECTORY STRUCTURE in your child theme buddypress/buddypress.php
+ * - Want to do more with BuddyPress? Duplicate the following directories to amend core CSS and JS:
+ * - buddypress/css
+ * - buddypress/js
+ * @package Wonderflux
+ */
+
+get_header();
+wfmain_before_wrapper(); //WF display hook
+
+wfmain_before_all_container(); //WF display hook
+wfmain_before_bp_container(); //WF display hook
+
+echo apply_filters( 'wflux_layout_content_container_open', '<div class="container" id="main-content">' );
+
+	// Main content
+	wfmain_before_all_content(); //WF display hook
+	wfmain_before_bp_content(); //WF display hook
+
+	get_template_part( 'loop', 'index' );
+
+	wfmain_after_bp_content(); //WF display hook
+	wfmain_after_all_content(); //WF display hook
+
+	wfx_get_sidebar(''); //WF WordPress get_sidebar function replacement
+
+	// Display hooks for after main content and sidebar
+	wfmain_after_bp_main_content(); //WF display hook
+	wfmain_after_all_main_content(); //WF display hook
+
+echo apply_filters( 'wflux_layout_content_container_close', '</div>' );
+
+wfmain_after_bp_container(); //WF display hook
+wfmain_after_all_container(); //WF display hook
+
+wfmain_after_wrapper(); //WF display hook
+
+get_footer();
+?>

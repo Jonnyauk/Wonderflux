5765257e89234e8df528912a7cc52b2caba2dc05
Jonnyauk <jonny@wonderflux.com>
2013-10-14 08:44:47 +0100

    Amend BuddyPress documentation
    Amend documentation to provide a bit more help for those using BuddyPress.

--- buddypress/buddypress.php
+++ buddypress/buddypress.php
@@ -1,13 +1,25 @@
 <?php
 /*
- * Core Wonderflux buddypress template
+ * Core Wonderflux BuddyPress template
  * Customise this in your child theme by:
  * - Using hooks and your own functions
  * - Copying this file to your child theme - it will over-ride this file
  * - IMPORTANT: DUPLICATE DIRECTORY STRUCTURE in your child theme buddypress/buddypress.php
- * - Want to do more with BuddyPress? Duplicate the following directories to amend core CSS and JS:
+ * - This file acts as a 'wrapper' around 
+ * 
+ * - Want to do more with BuddyPress? 
+ * - Duplicate the following directories to amend core CSS and JS:
  * - buddypress/css
  * - buddypress/js
+ * 
+ * - Want to do even more with BuddyPress template files?
+ * - Duplicate the following directories into your child theme and have fun!
+ * - buddypress/activity
+ * - buddypress/blogs
+ * - buddypress/forums
+ * - buddypress/groups
+ * - buddypress/members
+ * 
  * @package Wonderflux
  */
 

f8f095c1df21401691c68b7000e7254b341f987e
Jonnyauk <jonny@wonderflux.com>
2014-01-08 12:21:19 +0000

    Update compatibility to WordPress 3.8
    All testing complete - works perfectly no changes required... cool!

--- wf-includes/wf-version.php
+++ wf-includes/wf-version.php
@@ -21,5 +21,5 @@ define( 'WF_WORDPRESS_MIN', '3.2' );
 /**
  * The optimum recommended version of WordPress for running this version of Wonderflux
  */
-define( 'WF_WORDPRESS_OPTI', '3.7' );
+define( 'WF_WORDPRESS_OPTI', '3.8' );
 ?>

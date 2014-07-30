f13605606be7c51d004f3e26ede9347c36fdc395
Jonnyauk <jonny@wonderflux.com>
2014-07-30 08:46:47 +0100

    Change first Wonderflux options page content
    Change text content.

--- wf-includes/admin-pages/wf-page-core.php
+++ wf-includes/admin-pages/wf-page-core.php
@@ -5,28 +5,39 @@
 	$ct = ( WF_WORDPRESS_VERSION < 3.4 ) ? current_theme_info() : wp_get_theme();
 	?>
 
-	<h3><?php esc_attr_e('Current Wonderflux Child Theme', 'wonderflux'); ?></h3>
+	<h2><?php esc_attr_e('Welcome to Wonderflux', 'wonderflux'); ?></h2>
+	<div class="clear"></div>
+	<p>
+	<?php esc_attr_e('Wonderflux is an advanced, open source theme framework. ', 'wonderflux'); ?>
+	<?php esc_attr_e('It is built to provide a solid, reliable and easily updateable base for creating bespoke WordPress themes.', 'wonderflux'); ?>
+	</p>
+	<p>
+	<?php esc_attr_e('The best way to use Wonderflux is to create a child theme. ', 'wonderflux'); ?>
+	<?php esc_attr_e('Explore the links in the help and support section at the bottom of this page to find out more.', 'wonderflux'); ?>
+	</p>
+
+	<h2><?php esc_attr_e('Current Wonderflux Child Theme', 'wonderflux'); ?></h2>
+	<div class="clear"></div>
 	<div id="current-wf-theme">
 
 		<?php if ( $screenshot = $ct->get_screenshot() ) : ?>
-			<img src="<?php echo esc_url( $screenshot ); ?>" style="float:left; margin: 0 20px 20px 0;" />
+			<img src="<?php echo esc_url( $screenshot ); ?>" style="float:left; margin: 0 20px 20px 0; max-height:130px;" />
 		<?php endif; ?>
 
 		<h4><?php
-			/* translators: 1: theme title, 2: theme version, 3: theme author */
 			printf(__('%1$s %2$s by %3$s'), $ct->title, $ct->version, $ct->author) ; ?></h4>
 		<p class="theme-description"><?php echo $ct->description; ?></p>
-		<?php echo '<p>' . esc_attr__('The theme files are located in', 'wonderflux') . ' <code>'. WF_THEME_URL .'</code></p>'; ?>
+		<?php echo '<p>' . esc_attr__('The theme files are located in', 'wonderflux') . ' <code>'. WF_THEME_URL .'/</code></p>'; ?>
 
 		<div class="clear"></div>
 
 		<?php
 		// Backpat - depreciated function get_current_theme() in WordPress 3.4
-		$this_current_theme = ( WF_WORDPRESS_VERSION < 3.4 ) ? get_current_theme() : wp_get_theme()->Name;
+		//$this_current_theme = ( WF_WORDPRESS_VERSION < 3.4 ) ? get_current_theme() : wp_get_theme()->Name;
 
-		echo '<h2>' . apply_filters( 'wflux_options_theme_name_header', $this_current_theme ) . ' ' . esc_attr__('display functions', 'wonderflux') . '</h2>';
-		echo '<div class="clear"></div>';
-		echo '<p>' . esc_attr__('COMING SOON - Theme designers will be able to enable options to control theme scripts functionality.', 'wonderflux') . '</p>';
+		//echo '<h2>' . apply_filters( 'wflux_options_theme_name_header', $this_current_theme ) . ' ' . esc_attr__('display functions', 'wonderflux') . '</h2>';
+		//echo '<div class="clear"></div>';
+		//echo '<p>' . esc_attr__('COMING SOON - Theme designers will be able to enable options to control theme scripts functionality.', 'wonderflux') . '</p>';
 		?>
 	</div>
 

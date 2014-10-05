bdb47a9d2a446d8045ed20ff671582933de14e64
Jonnyauk <jonny@wonderflux.com>
2014-10-06 00:06:35 +0100

    Flux Layout - simplify generators
    Simplify generators for relative sizes and media query specific min sizes - these will likely be defaults if no options set - reduce code bloat.

--- wf-content/css/wf-css-flux-layout.php
+++ wf-content/css/wf-css-flux-layout.php
@@ -143,9 +143,9 @@ class wflux_layout {
 		$this->rwd_columns = ( is_numeric( $_GET['c'] ) && $_GET['c'] <= 101 ) ? $_GET['c'] : 16;
 		$this->rwd_class_prepend = ( !isset($this->rwd_class_prepend) ) ? 'box-': strtolower( preg_replace('/[^a-z0-9_\-]/', '', $this->rwd_class_prepend) );
 		$this->rwd_column_width = 100 / $this->rwd_columns;
-		$this->rwd_relative = array(1,2,3,4,5,6,7,8,9,10,11,12,16,32);
-		$this->mq_specific = array(2,4,8,16);
-		$this->rwd_minify = "\n";
+		$this->rwd_relative = array(1,2,3,4,6,8,12,16);
+		$this->mq_specific = array(2,3,4,8);
+
 		$this->mq_config = array(
 			'tiny'	=> array(
 							'def'	=> 'mq-tiny',

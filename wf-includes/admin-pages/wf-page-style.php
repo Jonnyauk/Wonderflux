48cf84beb484c3ae8c64d2f899a098a3564eae21
Jonnyauk <jonny@wonderflux.com>
2014-10-17 23:19:00 +0100

    Cleanup stylelab page
    Simplify text and setup buttons for translation.

--- wf-includes/admin-pages/wf-page-style.php
+++ wf-includes/admin-pages/wf-page-style.php
@@ -1,29 +1,27 @@
 <div class="wrap">
+
 	<form action="options.php" method="post">
-	<h2><?php esc_attr_e('Theme layout configuration', 'wonderflux'); ?></h2>
-	<div class="clear"></div>
-	<p>
-	<?php esc_attr_e('It is currently possible to set layout sizes that do not work correctly in the layout cloumn/grid settings. For suggested size configurations, click on the help tab top right.', 'wonderflux'); ?>
-	</p>
-	<p><strong>
-	<?php esc_attr_e('NOTE: For suggested size configurations, click on the help tab top right.', 'wonderflux'); ?>
-	</p></strong>
-	<p>
-	<?php esc_attr_e('You can filter most of these values in your child theme, allowing finer grain control (for instance, conditional on page/type of view). Filters will over-ride any values set here.', 'wonderflux'); ?>
-	</p>
 
-	<p class="submit">
-		<input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e('Save Template settings'); ?>" />
-	</p>
+		<h2><?php esc_attr_e('Theme layout configuration', 'wonderflux'); ?></h2>
 
-	<div id="wfx_fields_display">
-		<?php settings_fields('wf_settings_display'); ?>
-		<?php do_settings_sections('wonderflux_stylelab_grid'); ?>
-		<?php do_settings_sections('wonderflux_stylelab'); ?>
-	</div>
+		<div class="clear"></div>
 
-	<p class="submit">
-		<input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e('Save Template settings'); ?>" />
-	</p>
+		<p>
+		<?php esc_attr_e('Configure the dynamic CSS layout (note that filters over-ride these options.)', 'wonderflux'); ?>
+		</p>
+
+		<div id="wfx_fields_display">
+			<?php
+			settings_fields('wf_settings_display');
+			do_settings_sections('wonderflux_stylelab_grid');
+			do_settings_sections('wonderflux_stylelab');
+			?>
+		</div>
+
+		<p class="submit">
+			<input name="Submit" type="submit" class="button-primary" value="<?php esc_attr_e('Save settings', 'wonderflux'); ?>" />
+		</p>
+
 	</form>
+
 </div>

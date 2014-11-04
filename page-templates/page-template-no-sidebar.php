553ee2cf4c1896a0bf6c69775ef9f0ef740c74a0
Jonnyauk <jonny@wonderflux.com>
2014-11-04 19:41:54 +0000

    No sidebar page template
    Add a no sidebar page template.

---
+++ page-templates/page-template-no-sidebar.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: No Sidebar
+ *
+ * Wonderflux page template - no sidebar
+ *
+ * @package Wonderflux
+ */
+
+
+add_filter('wflux_sidebar_1_display', 'wfx__N');
+get_template_part('page');
+?>

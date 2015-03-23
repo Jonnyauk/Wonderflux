6e10b5387bea1c885e75fb75c469787eefaec1a1
Jonnyauk <jonny@wonderflux.com>
2015-03-23 15:06:42 +0000

    Update WordPress version string
    Update compatibility check string.

--- wf-includes/wf-version.php
+++ wf-includes/wf-version.php
@@ -21,5 +21,5 @@ define( 'WF_WORDPRESS_MIN', '3.4' );
 /**
  * The optimum recommended version of WordPress for running this version of Wonderflux
  */
-define( 'WF_WORDPRESS_OPTI', '4.1' );
+define( 'WF_WORDPRESS_OPTI', '4.1.1' );
 ?>

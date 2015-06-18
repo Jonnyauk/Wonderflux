8697d9078ffbdeed548b37ccb61dfab15cad4cce
Jonnyauk <Jonnyauk@users.noreply.github.com>
2015-06-18 16:47:58 +0100

    Remove bug in IE8 rendering
    Remove legacy code - basic browser detection is added to the body class by Wonderflux already, so this is not needed. Also bug in conditional tag structure meant code comment appeared in page output in IE8 (it tried it's best).

--- header.php
+++ header.php
@@ -15,19 +15,7 @@ wf_output_start(); //WF display hook
 
 ?>
 
-<!--[if IE 7]>
-<html class="ie ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html class="ie ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 9]>
-<html class="ie ie9" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[ if !(IE 7) | !(IE 8) | !(IE 9) ]><!-->
 <html <?php language_attributes(); ?>>
-<!--<![endif]-->
-
 <head>
 <?php
 wf_head_meta();

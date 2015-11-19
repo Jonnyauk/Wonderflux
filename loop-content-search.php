46ed9c843ff97066cbfe696b6c62ac2c4f1a80b7
Jonnyauk <Jonnyauk@users.noreply.github.com>
2015-11-19 00:57:18 +0000

    Display post type name in search loop
    Corrects bug with multiple word post types - this gets the proper post type name and prepends it to the title in search results.

--- loop-content-search.php
+++ loop-content-search.php
@@ -14,11 +14,13 @@
 	<div class="entry-content">
 
 		<?php
-		$this_pt = get_post_type();
-		$this_pt = (!empty($this_pt)) ? ucfirst($this_pt) : false;
+		// Setup post name to prepend to title
+		$this_pt_detail = get_post_type_object( get_post_type(get_the_ID()) );
+		$this_pt_detail = ( isset($this_pt_detail->labels->singular_name) && !empty($this_pt_detail->labels->singular_name) ) ? $this_pt_detail->labels->singular_name : '';
+		$pt_title_prepend = ( !empty($this_pt_detail) ) ? ucfirst( $this_pt_detail ) . ': ' : '';
 		?>
 
-		<h2 class="archive-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Read %s', 'wonderflux' ), the_title_attribute( 'echo=0' ) ); ?>"><?php echo esc_html( $this_pt ) . ': '; the_title(); ?></a></h2>
+		<h2 class="archive-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Read %s', 'wonderflux' ), the_title_attribute( 'echo=0' ) ); ?>"><?php echo esc_html( $pt_title_prepend ); the_title(); ?></a></h2>
 
 		<p><?php wfx_excerpt('limit=20'); ?></p>
 

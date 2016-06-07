6f992335c53b3188f2bca4cdb9313251c38bdd0f
Jonnyauk <Jonnyauk@users.noreply.github.com>
2016-06-07 09:32:49 +0100

    Deal with no value for range
    If no value lets set defaults ;)

--- wf-includes/wf-helper-functions.php
+++ wf-includes/wf-helper-functions.php
@@ -136,7 +136,7 @@ class wflux_data {
 		$this->wfx_range_core = (isset($this->wfx_db_display['range_core']) ) ? $this->wfx_db_display['range_core'] : false;
 		// Validate
 		$wfx_range_core_out = '1-2-4-5-8-10';
-		if ( is_string($this->wfx_range_core) && ($this->wfx_range_core != $wfx_range_core_out) ) {
+		if ( !empty($this->wfx_range_core) && is_string($this->wfx_range_core) && ($this->wfx_range_core != $wfx_range_core_out) ) {
 			$wfx_range_core_out = preg_replace( '/\s+/', '', esc_attr($this->wfx_range_core) );
 			$wfx_range_core_out = preg_replace( '/-[a-z]/i', '', $wfx_range_core_out );
 		}

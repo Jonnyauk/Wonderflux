344885490a0ed2d87ff7fd707c2dcca2be696b13
Jonnyauk <Jonnyauk@users.noreply.github.com>
2016-07-13 20:46:52 +0100

    Template file readme refactoring

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,12 @@
 <?php
 /**
- * Wonderflux sidebar content template
+ * Wonderflux primary sidebar template
  *
- * This will be over-ridden if you create a file of the same name in your child theme
+ * Customise this in your child theme by:
+ * - Using the Wonderflux hooks in this file - there are file specific and general ones
+ * - Using the 'sidebar-content' template part 'sidebar-content-404.php' or 'sidebar-content.php' (fallback if location specific file not available)
+ * - Copying this file to your child theme and amending - it will automatically be used instead of this file
+ * - IMPORTANT - if you do this, ensure you keep all Wonderflux hooks intact!
  *
  * @package Wonderflux
  */

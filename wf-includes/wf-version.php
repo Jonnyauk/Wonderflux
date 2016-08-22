f9fca69d5bdb1545eda95af3d992b6e4c56b0b01
Jonnyauk <Jonnyauk@users.noreply.github.com>
2016-08-22 14:10:02 +0100

    Version up! 2.5 begins…

--- wf-includes/wf-version.php
+++ wf-includes/wf-version.php
@@ -11,7 +11,7 @@ define( 'WF_WORDPRESS_VERSION', get_bloginfo( 'version' ) );
 /**
  * Wonderflux version string
  */
-define( 'WF_VERSION', '2.4' );
+define( 'WF_VERSION', '2.5' );
 
 /**
  * The minimum recommended version of WordPress for running this version of Wonderflux

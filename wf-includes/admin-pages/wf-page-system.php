ea88754ff36fdb6ac2292e6e35c283baaab06d4b
Jonnyauk <Jonnyauk@users.noreply.github.com>
2016-09-01 10:00:42 +0100

    Improve system page output
    Improve styling of constants.

--- wf-includes/admin-pages/wf-page-system.php
+++ wf-includes/admin-pages/wf-page-system.php
@@ -10,22 +10,22 @@ echo '<p>' . sprintf( esc_html__( 'PHP version installed: %s', 'wonderflux' ), p
 echo '<p>' . sprintf( esc_html__( 'MySQL version installed: %s', 'wonderflux' ), $wpdb->db_version() ) . '</p>';
 
 echo '<h3>' . esc_html__('Wonderflux contants you can use/define in your child theme', 'wonderflux') . '</h3>';
-echo '<p>' . sprintf( esc_html__( 'WF_MAIN_DIR: %s', 'wonderflux' ), WF_MAIN_DIR ) . '</p>';
-echo '<p>' . sprintf( esc_html__( 'WF_MAIN_URL: %s', 'wonderflux' ), WF_MAIN_URL ) . '</p>';
-echo '<p>' . sprintf( esc_html__( 'WF_CONTENT_DIR: %s', 'wonderflux' ), WF_CONTENT_DIR ) . '</p>';
-echo '<p>' . sprintf( esc_html__( 'WF_CONTENT_URL: %s', 'wonderflux' ), WF_CONTENT_URL ) . '</p>';
-echo '<p>' . sprintf( esc_html__( 'WF_INCLUDES_DIR: %s', 'wonderflux' ), WF_INCLUDES_DIR ) . '</p>';
-echo '<p>' . sprintf( esc_html__( 'WF_INCLUDES_URL: %s', 'wonderflux' ), WF_INCLUDES_URL ) . '</p>';
-echo '<p>' . sprintf( esc_html__( 'WF_THEME_URL: %s', 'wonderflux' ), WF_THEME_URL ) . '</p>';
-echo '<p>' . sprintf( esc_html__( 'WF_THEME_DIR: %s', 'wonderflux' ), WF_THEME_DIR ) . '</p>';
-echo '<p>' . sprintf( esc_html__( 'WF_ADMIN_ACCESS: %s', 'wonderflux' ), WF_ADMIN_ACCESS ) . '</p>';
+echo '<p><strong>' . sprintf( esc_html__( 'WF_MAIN_DIR: %s', 'wonderflux' ), '<code>' . WF_MAIN_DIR . '</code>' ) . '</strong></p>';
+echo '<p><strong>' . sprintf( esc_html__( 'WF_MAIN_URL: %s', 'wonderflux' ), '<code>' . WF_MAIN_URL . '</code>' ) . '</strong></p>';
+echo '<p><strong>' . sprintf( esc_html__( 'WF_CONTENT_DIR: %s', 'wonderflux' ), '<code>' . WF_CONTENT_DIR . '</code>' ) . '</strong></p>';
+echo '<p><strong>' . sprintf( esc_html__( 'WF_CONTENT_URL: %s', 'wonderflux' ), '<code>' . WF_CONTENT_URL . '</code>' ) . '</strong></p>';
+echo '<p><strong>' . sprintf( esc_html__( 'WF_INCLUDES_DIR: %s', 'wonderflux' ), '<code>' . WF_INCLUDES_DIR . '</code>' ) . '</strong></p>';
+echo '<p><strong>' . sprintf( esc_html__( 'WF_INCLUDES_URL: %s', 'wonderflux' ), '<code>' . WF_INCLUDES_URL . '</code>' ) . '</strong></p>';
+echo '<p><strong>' . sprintf( esc_html__( 'WF_THEME_URL: %s', 'wonderflux' ), '<code>' . WF_THEME_URL . '</code>' ) . '</strong></p>';
+echo '<p><strong>' . sprintf( esc_html__( 'WF_THEME_DIR: %s', 'wonderflux' ), '<code>' . WF_THEME_DIR . '</code>' ) . '</strong></p>';
+echo '<p><strong>' . sprintf( esc_html__( 'WF_ADMIN_ACCESS: %s', 'wonderflux' ), '<code>' . WF_ADMIN_ACCESS . '</code>' ) . '</strong></p>';
 
-echo '<h3>' . esc_html__('Other Wonderflux contants you can use in your child theme functions.php', 'wonderflux') . '</h3>';
+echo '<h3>' . esc_html__('Other Wonderflux contants you can use in your child theme', 'wonderflux') . '</h3>';
 echo '<p>' . esc_html__( 'Note that these only accept boolean values - true or false', 'wonderflux' ) . '</p>';
 $fw_replace = ( WF_THEME_FRAMEWORK_REPLACE === true ) ? 'true' : 'false';
 $fw_none = ( WF_THEME_FRAMEWORK_NONE === true ) ? 'true' : 'false';
 $fw_debug = ( WF_DEBUG === true ) ? 'true' : 'false';
-echo '<p>' . sprintf( esc_html__( 'WF_THEME_FRAMEWORK_REPLACE: %s', 'wonderflux' ), $fw_replace ) . '</p>';
-echo '<p>' . sprintf( esc_html__( 'WF_THEME_FRAMEWORK_NONE: %s', 'wonderflux' ), $fw_none ) . '</p>';
-echo '<p>' . sprintf( esc_html__( 'WF_DEBUG: %s', 'wonderflux' ), $fw_debug ) . '</p>';
+echo '<p><strong>' . sprintf( esc_html__( 'WF_THEME_FRAMEWORK_REPLACE: %s', 'wonderflux' ), '<code>' . $fw_replace . '</code>' ) . '</strong></p>';
+echo '<p><strong>' . sprintf( esc_html__( 'WF_THEME_FRAMEWORK_NONE: %s', 'wonderflux' ), '<code>' . $fw_none . '</code>' ) . '</strong></p>';
+echo '<p><strong>' . sprintf( esc_html__( 'WF_DEBUG: %s', 'wonderflux' ), '<code>' . $fw_debug . '</code>' ) . '</strong></p>';
 ?>

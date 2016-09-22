4b0d50b4c1c52f0dcd973b04fda2bb1508025b68
Jonny <contact@jonnya.net>
2016-09-22 18:33:54 +0100

    Search form code refactoring
    Just a little tidying - tabs not spaces thanks buddy!

--- searchform.php
+++ searchform.php
@@ -9,9 +9,9 @@
  */
 ?>
 <form id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
-  <fieldset>
-    <label for="search"><?php esc_attr_e( 'Search', 'wonderflux' ); ?></label>
-    <input type="text" name="s" class="field" id="search" value="<?php the_search_query(); ?>" />
-    <input type="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'wonderflux' ); ?>" />
-  </fieldset>
+	<fieldset>
+		<label for="search"><?php esc_attr_e( 'Search', 'wonderflux' ); ?></label>
+		<input type="text" name="s" class="field" id="search" value="<?php the_search_query(); ?>" />
+		<input type="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'wonderflux' ); ?>" />
+	</fieldset>
 </form>

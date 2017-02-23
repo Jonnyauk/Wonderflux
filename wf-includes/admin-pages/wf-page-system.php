8414072127d8f55e689c19dcf98fbd53d453cd5e
Jonny <contact@jonnya.net>
2017-02-23 22:55:53 +0000

    System page improvements
    Fix admin typo (contants/constants!) and improve system page text to make more helpful.

--- wf-includes/admin-pages/wf-page-system.php
+++ wf-includes/admin-pages/wf-page-system.php
@@ -1,15 +1,19 @@
 <?php
 $this->wf_latest_version_notice();
-
 global $wp_version;
 global $wpdb;
+
 echo '<h3>'. esc_html__('Technical and server information', 'wonderflux') . '</h3>';
+
 echo '<p>' . sprintf( esc_html__( 'Wonderflux version installed: %1$s, requires at-least WordPress v%2$s, optimised for WordPress v%3$s', 'wonderflux' ), WF_VERSION, WF_WORDPRESS_MIN, WF_WORDPRESS_OPTI ) . '</p>';
 echo '<p>' . sprintf( esc_html__( 'WordPress version installed: %s', 'wonderflux' ), $wp_version ) . '</p>';
 echo '<p>' . sprintf( esc_html__( 'PHP version installed: %s', 'wonderflux' ), phpversion() ) . '</p>';
 echo '<p>' . sprintf( esc_html__( 'MySQL version installed: %s', 'wonderflux' ), $wpdb->db_version() ) . '</p>';
 
-echo '<h3>' . esc_html__('Wonderflux contants you can use/define in your child theme', 'wonderflux') . '</h3>';
+echo '<h1>' . esc_html__('Constants you can define in your child theme', 'wonderflux') . '</h1>';
+
+echo '<h3>' . esc_html__('Wonderflux file paths', 'wonderflux') . '</h3>';
+
 echo '<p><strong>' . sprintf( esc_html__( 'WF_MAIN_DIR: %s', 'wonderflux' ), '<code>' . WF_MAIN_DIR . '</code>' ) . '</strong></p>';
 echo '<p><strong>' . sprintf( esc_html__( 'WF_MAIN_URL: %s', 'wonderflux' ), '<code>' . WF_MAIN_URL . '</code>' ) . '</strong></p>';
 echo '<p><strong>' . sprintf( esc_html__( 'WF_CONTENT_DIR: %s', 'wonderflux' ), '<code>' . WF_CONTENT_DIR . '</code>' ) . '</strong></p>';
@@ -20,8 +24,8 @@ echo '<p><strong>' . sprintf( esc_html__( 'WF_THEME_URL: %s', 'wonderflux' ), '<
 echo '<p><strong>' . sprintf( esc_html__( 'WF_THEME_DIR: %s', 'wonderflux' ), '<code>' . WF_THEME_DIR . '</code>' ) . '</strong></p>';
 echo '<p><strong>' . sprintf( esc_html__( 'WF_ADMIN_ACCESS: %s', 'wonderflux' ), '<code>' . WF_ADMIN_ACCESS . '</code>' ) . '</strong></p>';
 
-echo '<h3>' . esc_html__('Other Wonderflux contants you can use in your child theme', 'wonderflux') . '</h3>';
-echo '<p>' . esc_html__( 'Note that these only accept boolean values - true or false', 'wonderflux' ) . '</p>';
+echo '<h3>' . esc_html__('Wonderflux debug and Flux Layout CSS framework control', 'wonderflux') . '</h3>';
+
 $fw_replace = ( WF_THEME_FRAMEWORK_REPLACE === true ) ? 'true' : 'false';
 $fw_none = ( WF_THEME_FRAMEWORK_NONE === true ) ? 'true' : 'false';
 $fw_debug = ( WF_DEBUG === true ) ? 'true' : 'false';

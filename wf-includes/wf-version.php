3b8f1e952fa6500ffe395b88065039499abcc0fc
Jonny <contact@jonnya.net>
2017-04-30 02:55:41 +0100

    Version up!

--- wf-includes/wf-version.php
+++ wf-includes/wf-version.php
@@ -21,5 +21,5 @@ define( 'WF_WORDPRESS_MIN', '4.0' );
 /**
  * The optimum recommended version of WordPress for running this version of Wonderflux
  */
-define( 'WF_WORDPRESS_OPTI', '4.6' );
+define( 'WF_WORDPRESS_OPTI', '4.7.x' );
 ?>
